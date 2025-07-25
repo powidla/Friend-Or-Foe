c2a95b4f7040f1ce7674d87e819b0cd5fa874969
powidla <109307554+powidla@users.noreply.github.com>
2025-07-25 12:33:23 +0200

    Update __init__.py

--- friend_or_foe/__init__.py
+++ friend_or_foe/__init__.py
@@ -1,13 +1,11 @@
-"""
+'''
 Friend-Or-Foe: A collection of microbial datasets obtained from metabolic modeling.
 
 This package provides easy access to microbial interaction datasets for machine learning research,
 along with utilities and model implementations for predictive modeling of microbial interactions.
-"""
+'''
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
-__email__ = "your.email@example.com"
 
 from .data.loader import FriendOrFoeDataLoader
 from .models.base import BaseModel
