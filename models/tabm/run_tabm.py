3fd380294c2a966d91b53b9ed8677f99e2364b10
powidla <109307554+powidla@users.noreply.github.com>
2026-06-18 13:37:57 +0200

    Update run_tabm.py

--- models/tabm/run_tabm.py
+++ models/tabm/run_tabm.py
@@ -8,4 +8,4 @@ for col in COLS:
     for gr in GRS:
         for ds in DSS:
             print(f"\n{'='*60}\nRunning COL={col}  GR={gr}  DS={ds}\n{'='*60}")
-            subprocess.run([sys.executable, "tabm.py", "--col", col, "--gr", gr, "--ds", ds], check=True)
+            subprocess.run([sys.executable, "core.py", "--col", col, "--gr", gr, "--ds", ds], check=True)
