5c3d3ab571197dd16f9ceba6c12ea7275aba0764
powidla <109307554+powidla@users.noreply.github.com>
2026-06-18 13:32:24 +0200

    Create run_tabm.py

---
+++ models/tabm/run_tabm.py
@@ -0,0 +1,11 @@
+import subprocess, sys
+ 
+COLS = ["AGORA", "CARVEME"]
+GRS  = ["100", "50"]
+DSS  = ["TL-I", "TL-II"]
+ 
+for col in COLS:
+    for gr in GRS:
+        for ds in DSS:
+            print(f"\n{'='*60}\nRunning COL={col}  GR={gr}  DS={ds}\n{'='*60}")
+            subprocess.run([sys.executable, "tabm.py", "--col", col, "--gr", gr, "--ds", ds], check=True)
